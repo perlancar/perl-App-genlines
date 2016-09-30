2456f4596db59a0747f37982ed91c564f2ce9645
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-30 19:55:20 +0700

    Fixes

---
+++ lib/App/genlines.pm
@@ -0,0 +1,18 @@
+package App::genlines;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Produce lines with a specified speed range
+
+=head1 SYNOPSIS
+
+See the command-line script L<genlines>.
+
+
+=head1 SEE ALSO
+
+L<linespeed>
+
+=cut
